0d55b88d291b5edd6aad374a73cb9425e2c88702
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 18:07:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :"newteam"
+  end
 
+  post '/team' do
+    @team = Team.new 
+    
+    erb :"team" 
+  end
 
 end
