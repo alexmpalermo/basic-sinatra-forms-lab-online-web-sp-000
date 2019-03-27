d11eff17a3d7fa299a7370edabdfaf4a625f392b
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-27 18:29:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     @team = Team.new 
     @team.name = params[:name]
     @team.coach = params[:coach]
-    @team.point = params[:point]
+    @team.point = params[:pg]
     @team.shooting = params[:shooting]
     @team.smallfwd = params[:smallfwd]
     @team.powerfwd = params[:powerfwd]
